0d0f34e9881eb70de8ce0a3e9acb9d31386ef240
lawand7 <lwand.allo@gmail.com>
2022-02-18 03:38:57 +0100

    implement color picker

--- briar/smart-moderation-app/src/main/java/dhbw/smartmoderation/moderationcards/overview/ModerationCardsFragment.java
+++ briar/smart-moderation-app/src/main/java/dhbw/smartmoderation/moderationcards/overview/ModerationCardsFragment.java
@@ -1,8 +1,10 @@
 package dhbw.smartmoderation.moderationcards.overview;
 
 import android.app.AlertDialog;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -12,6 +14,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.pes.androidmaterialcolorpickerdialog.ColorPicker;
 
 import dhbw.smartmoderation.R;
 
@@ -19,27 +22,48 @@ public class ModerationCardsFragment extends Fragment {
     private View view;
     private ModerationCardsController controller;
     private FloatingActionButton addButton;
+    private Button pickColorButton;
+    private SurfaceView cardColorViewer;
+    private int cardColor;
     private View popUp;
 
-    private View.OnClickListener addButtonClickListener = v -> {
+
+    private final View.OnClickListener pickColorButtonClickListener = v -> {
+        ColorPicker colorPicker = new ColorPicker(getActivity());
+        colorPicker.show();
+        colorPicker.enableAutoClose();
+        colorPicker.setCallback(color->{
+            cardColor = color;
+            cardColorViewer.setBackgroundColor(cardColor);
+        });
+    };
+
+    private final View.OnClickListener addButtonClickListener = v -> {
         LayoutInflater inflater = LayoutInflater.from(getActivity());
         popUp = inflater.inflate(R.layout.popup_create_moderation_card, null);
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setView(this.popUp);
         AlertDialog alertDialog = builder.create();
+
+        pickColorButton = this.popUp.findViewById(R.id.pickColorButton);
+        pickColorButton.setOnClickListener(pickColorButtonClickListener);
+        cardColorViewer = this.popUp.findViewById(R.id.colorViewer);
         Button addButton = this.popUp.findViewById(R.id.addButton);
         addButton.setOnClickListener(view ->{
             //todo: get parameters for method addModerationCard()
             controller.addModerationCard();
+            alertDialog.cancel();
         });
         Button cancelButton = this.popUp.findViewById(R.id.cancelButton);
         cancelButton.setOnClickListener( view -> alertDialog.cancel());
         alertDialog.show();
     };
 
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        controller = new ModerationCardsController();
     }
 
 
