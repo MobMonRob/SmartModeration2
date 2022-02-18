d4da8d08e0c011be0c94a5d123a917c3ee5f33cb
lawand7 <lwand.allo@gmail.com>
2022-02-18 01:55:19 +0100

    implement add moderation card button

--- briar/smart-moderation-app/src/main/java/dhbw/smartmoderation/moderationcards/overview/ModerationCardsController.java
+++ briar/smart-moderation-app/src/main/java/dhbw/smartmoderation/moderationcards/overview/ModerationCardsController.java
@@ -1,4 +1,8 @@
 package dhbw.smartmoderation.moderationcards.overview;
 
 public class ModerationCardsController {
+    //todo
+    public void addModerationCard() {
+
+    }
 }
--- briar/smart-moderation-app/src/main/java/dhbw/smartmoderation/moderationcards/overview/ModerationCardsFragment.java
+++ briar/smart-moderation-app/src/main/java/dhbw/smartmoderation/moderationcards/overview/ModerationCardsFragment.java
@@ -1,18 +1,42 @@
 package dhbw.smartmoderation.moderationcards.overview;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import dhbw.smartmoderation.R;
 
 public class ModerationCardsFragment extends Fragment {
     private View view;
+    private ModerationCardsController controller;
+    private FloatingActionButton addButton;
+    private View popUp;
+
+    private View.OnClickListener addButtonClickListener = v -> {
+        LayoutInflater inflater = LayoutInflater.from(getActivity());
+        popUp = inflater.inflate(R.layout.popup_create_moderation_card, null);
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setView(this.popUp);
+        AlertDialog alertDialog = builder.create();
+        Button addButton = this.popUp.findViewById(R.id.addButton);
+        addButton.setOnClickListener(view ->{
+            //todo: get parameters for method addModerationCard()
+            controller.addModerationCard();
+        });
+        Button cancelButton = this.popUp.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener( view -> alertDialog.cancel());
+        alertDialog.show();
+    };
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +49,8 @@ public class ModerationCardsFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         this.view = inflater.inflate(R.layout.fragment_moderation_cards, container, false);
         getActivity().setTitle(getString(R.string.moderationCardTitle));
+        addButton = this.view.findViewById(R.id.floatingActionButton);
+        this.addButton.setOnClickListener(addButtonClickListener);
         return this.view;
     }
 }
